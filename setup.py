363af75af1a05f5e74df092264e289a6eb8bef69
Asaf Peer <asafpr@gmail.com>
2015-05-21 18:05:25 +0300

    Readme fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='pro_clash',
-      version='0.12',
+      version='0.13',
       description='Processing PRO-CLASH experiments results',
       long_description=readme(),
       classifiers=[

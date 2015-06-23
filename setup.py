8f180083d8496e7db1591aa125b0217d624877c7
Asaf Peer <asafpr@gmail.com>
2015-06-23 13:48:41 +0300

    changed aln params

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='pro_clash',
-      version='0.25',
+      version='0.26',
       description='Processing PRO-CLASH experiments results',
       long_description=readme(),
       classifiers=[
